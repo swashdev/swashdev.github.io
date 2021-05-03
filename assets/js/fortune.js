c70f12dabe7bcfc7075eefc4716b2e16582c0cd7
swashberry <swashdev@pm.me>
2021-05-02 19:22:16 -0500

    add a fortune teller to the header.
    The site's subtitle will now be replaced with a random fortune from the
    "fortunes" file when the page is loaded.

---
+++ assets/js/fortune.js
@@ -0,0 +1,34 @@
+/* This file constitutes free and unencumbered software released into the
+ * public domain.  Anyone is free to copy, modify, publish, use, compile,
+ * sell, or distribute this software, for any purpose, commercial or
+ * non-commercial, and by any means.  This software is presented "as-is" and
+ * with no warantees or guarantees.
+ * For more information, see <https://unlicense.org>
+ */
+
+function populateFortune( xml )
+{
+	var cookieList, cookie, xmlDoc, txt;
+	xmlDoc = xml.responseXML;
+
+	cookieList = xmlDoc.getElementsByTagName("cookie");
+	var i = Math.floor(Math.random() * cookieList.length);
+	cookie = cookieList.item(i);
+
+	txt = cookie.getElementsByTagName( "fortune" ).item(0).textContent;
+
+	if (cookie.getElementsByTagName( "cite" ).length > 0) {
+		txt = txt + "<br /> — " + cookie.getElementsByTagName( "cite" ).item(0).textContent;
+	}
+
+	document.getElementById("fortune").innerHTML = txt;
+}
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			populateFortune(this);
+		}
+	};
+xmlhttp.open("GET", "/assets/xml/fortunes.xml", true);
+xmlhttp.send();
